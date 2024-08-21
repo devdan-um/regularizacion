26d7208a893f5e3c5b08c897b31228581d940c89
devdan <f.gomez@univermilenium.edu.mx>
2024-08-20 22:38:54 -0600

    proyecto inicial de regularizacion

--- src/main/java/com/univer/estudiantes/controller/EstudianteController.java
+++ src/main/java/com/univer/estudiantes/controller/EstudianteController.java
@@ -4,9 +4,8 @@ package com.univer.estudiantes.controller;
 import com.univer.estudiantes.entity.EstudianteEntity;
 import com.univer.estudiantes.repository.EstudianteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/univer")
@@ -24,6 +23,16 @@ public class EstudianteController {
         repository.save(entity);
 
         return "Estudiante guardado";
+    }
+
+    @PostMapping("/save")
+    public ResponseEntity guardarEstudiante(@RequestBody EstudianteEntity request){
+        EstudianteEntity entity = new EstudianteEntity();
+        entity.setNombre(request.getNombre());
+
+        repository.save(entity);
+
+        return ResponseEntity.ok("El estudiante se guardo de manera correcta");
     }
 
 
